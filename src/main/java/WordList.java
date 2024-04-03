52f1b993a99adca6a40ef9210573ac4b6ec62091
Brian0520 <143020505+Brian0520@users.noreply.github.com>
2024-04-03 09:33:25 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -10,13 +10,20 @@ public class WordList
 
   public int numWordsOfLength(int len)
   {
-    /* to be implemented in part (a) */
-    
+   int sum = 0;
+    for(int i = 0; i < myList.size(); i++)
+      if(myList.et(i) == len){
+        sum++;
+      }
+    return sum;
   }
 
   public void removeWordsOfLength(int len)
   {
-    /* to be implemented in part (b) */
-
+    for(int i = 0; i < myList.size(); i++)
+       if(myList.get(i) == len) {
+         myList.remove(i);
+       }
+  return myList
   }
 }
